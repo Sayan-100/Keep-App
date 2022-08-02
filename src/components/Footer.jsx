671da5ac54753af3f19d0275717ab625f48b2a9e
Sayan Biswas <bsayan289@gmail.com>
2022-08-02 18:42:40 +0530

    first-commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Footer()
+{
+
+    const currentYear = new Date().getFullYear();
+
+    return (
+        <footer>
+            <p>
+                Copyright &#169; {currentYear}
+            </p>
+        </footer>
+    )
+}
+
+export default Footer;
